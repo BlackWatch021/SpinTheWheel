20574191cf858fbdeae6ae53a8ec1e18cb91998a
Himanshu Negi <himanshu.21negi@gmail.com>
2022-12-04 23:14:51 +0530

    assets imported

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Ahoy</div>;
+  return (
+    <div className="main_container">
+      <div className="main_wrapper">
+        <div className="tablet_background">
+          <img src="./Assets/Images/background.png" alt="" />
+        </div>
+        <div className="main_body">
+          <img
+            className="desktop_back"
+            src="./Assets/Others/desktopBack.svg"
+            alt=""
+          />
+          <img
+            className="desktop_back"
+            src="./Assets/Others/desktopBackBottom.svg"
+            alt=""
+          />
+          <img
+            className="tablet_back"
+            src="./Assets/Others/tabletBack.svg"
+            alt=""
+          />
+          <img
+            className="tablet_back"
+            src="./Assets/Others/tabletBackBottom.svg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;

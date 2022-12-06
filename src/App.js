208682b714a2b2b0b891e4f6ca57dba744b5773a
Himanshu Negi <himanshu.21negi@gmail.com>
2022-12-06 23:28:46 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -28,6 +28,35 @@ function App() {
             src="./Assets/Others/tabletBackBottom.svg"
             alt=""
           />
+          <div className="main_content">
+            <img src="./Assets/Images/mockWheel.png" alt="" />
+            <div className="user_details">
+              <h2>
+                This is how EngageBud
+                <br /> looks like in action!
+              </h2>
+              <input type="email" name="email" />
+              <input type="tel" name="phone" />
+              <div className="terms_conditions">
+                <input type="checkbox" />
+                <span>
+                  I agree to receiving recurring automated messages at the
+                  number I have provided. Consent is not a condition to
+                  purchase.
+                </span>
+              </div>
+              <button type="submit">Try your luck</button>
+              <span>
+                *You can spin the wheel only once! *If you win, you can claim
+                your coupon for 10 minutes only!
+              </span>
+
+              <div className="skip_it">
+                <span>No, I don't feel lucky</span>
+                <img src="./Assets/Others/skip.svg" alt="" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>

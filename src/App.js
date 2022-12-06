98e635de984b13aa6f0e439c18931179bf880c82
Himanshu Negi <himanshu.21negi@gmail.com>
2022-12-07 00:38:09 +0530

    Landing page done

--- src/App.js
+++ src/App.js
@@ -28,51 +28,53 @@ function App() {
             src="./Assets/Others/tabletBackBottom.svg"
             alt=""
           />
-          <div className="main_content">
-            <img src="./Assets/Images/mockWheel.png" alt="" />
-            <div className="user_details">
-              <h2>
-                This is how EngageBud
-                <br /> looks like in action!
-              </h2>
-              <div className="input_fields">
-                <img
-                  className="input_icons"
-                  src="./Assets/Others/mail.svg"
-                  alt=""
-                />
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="something@engagebud"
-                />
-              </div>
-              <div className="input_fields">
-                <img
-                  className="input_icons"
-                  src="./Assets/Others/phone.svg"
-                  alt=""
-                />
-                <input type="tel" name="phone" placeholder="9004 23xxxx" />
-              </div>
+          <div className="main_content_wrapper">
+            <div className="main_content">
+              <img src="./Assets/Images/mockWheel.png" alt="" />
+              <div className="user_details">
+                <h1>
+                  This is how EngageBud
+                  <br /> looks like in action!
+                </h1>
+                <div className="input_fields">
+                  <img
+                    className="input_icons"
+                    src="./Assets/Others/mail.svg"
+                    alt=""
+                  />
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="something@engagebud"
+                  />
+                </div>
+                <div className="input_fields">
+                  <img
+                    className="input_icons"
+                    src="./Assets/Others/phone.svg"
+                    alt=""
+                  />
+                  <input type="tel" name="phone" placeholder="9004 23XXXX" />
+                </div>
 
-              <div className="terms_conditions">
-                <input className="check_the_box" type="checkbox" />
-                <span>
-                  I agree to receiving recurring automated messages at the
-                  number I have provided. Consent is not a condition to
-                  purchase.
+                <div className="terms_conditions">
+                  <input className="check_the_box" type="checkbox" />
+                  <span>
+                    I agree to receiving recurring automated messages at the
+                    number I have provided. Consent is not a condition to
+                    purchase.
+                  </span>
+                </div>
+                <button type="submit">Try your luck</button>
+                <span className="notice">
+                  *You can spin the wheel only once! *If you win, you can claim
+                  your coupon for 10 minutes only!
                 </span>
-              </div>
-              <button type="submit">Try your luck</button>
-              <span>
-                *You can spin the wheel only once! *If you win, you can claim
-                your coupon for 10 minutes only!
-              </span>
 
-              <div className="skip_it">
-                <span>No, I don't feel lucky</span>
-                <img src="./Assets/Others/skip.svg" alt="" />
+                <div className="skip_it">
+                  <span>No, I don't feel lucky</span>
+                  <img src="./Assets/Others/skip.svg" alt="" />
+                </div>
               </div>
             </div>
           </div>
